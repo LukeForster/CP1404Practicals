6626b142b49828b5066c53e4f993ce584a6a9a50
lukef <lukeforster6@gmail.com>
2022-10-10 19:34:43 +1000

    Add file list_exercises.py and update the subject_reader.py and string_formatting.py

--- Prac3/string_formatting.py
+++ Prac3/string_formatting.py
@@ -3,17 +3,16 @@
 name = "Gibson L-5 CES"
 year = 1922
 cost = 16035.4
-cost = int(cost)
 # print(cost)
 print(f"My {name} was first made in {year} (that's right, {year}!)")
-print(f'{year} {name} for about ${cost:,}')
+print(f'{year} {name} for about ${cost:,.0f}')
 
 print(' ')
 
 ## Using for loop
 
 for i in range(0, 200, 50):
-    print(f'{i:>}')
+    print(f'{i:4}')
 
 
 
--- Prac4/subject_reader.py
+++ Prac4/subject_reader.py
@@ -9,21 +9,35 @@ FILENAME = "subject_data.txt"
 def main():
     data = get_data()
     print(data)
+    display_subjects(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        # print(parts)  # See if that worked
+        data.append(parts)
+        # print("----------")
     input_file.close()
+    return data
+
+
+def display_subjects(data):
+    for subject_info in data:
+        # print(subject_info)
+        subject = subject_info[0]
+        teacher = subject_info[1]
+        students = subject_info[2]
+        # print("{} is taught by {:12} and has {:3} students".format(*subject_info))
+        print(f'{subject} is taught by {teacher} and has {students} students')
 
 
 main()
