0042b2ec70778b178403c072f83d6036e41e2c8a
lukef <lukeforster6@gmail.com>
2022-10-14 11:02:00 +1000

    Update file state_names.py

--- Prac5/state_names.py
+++ Prac5/state_names.py
@@ -6,18 +6,21 @@ File needs reformatting
 
 # This file has been reformatted so the dictionary code follows PEP 8 convention
 
-states = {"QLD": "Queensland",
-                "NSW": "New South Wales",
-                "NT" : "Northern Territory",
-                "WA" : "Western Australia",
-                "ACT": "Australian Capital Territory",
-                "VIC": "Victoria", "TAS": "Tasmania"}
-print(states)
+short_to_long_states = {"QLD": "Queensland",
+          "NSW": "New South Wales",
+          "NT": "Northern Territory",
+          "WA": "Western Australia",
+          "ACT": "Australian Capital Territory",
+          "VIC": "Victoria", "TAS": "Tasmania"}
+print(short_to_long_states)
 
-state = input("Enter short state: ").upper()
-while state != "":
-    if state in states:
-        print(state, "is", states[state])
+users_state = input("Enter short state: ").upper()
+while users_state != "":
+    if users_state in short_to_long_states:
+        print(users_state, "is", short_to_long_states[users_state])
     else:
         print("Invalid short state")
-    state = input("Enter short state: ").upper()
+    users_state = input("Enter short state: ").upper()
+
+for state_short,state_long in short_to_long_states.items():
+    print(f'{state_short} is {state_long}')
