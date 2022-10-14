2b8238ea653941418890d143c329cb3dc5b5c87d
lukef <lukeforster6@gmail.com>
2022-10-14 17:31:55 +1000

    Refactor emails.py

--- Prac5/emails.py
+++ Prac5/emails.py
@@ -1,20 +1,31 @@
 
 email_to_fullname = {}
 email = input('Email: ')
-while email != '':
+
+
+def main():
+    global email
+    while email != '':
+        fullname = get_fullname(email)
+        # print(email)
+        # print(fullname)
+        confirmation = input(f"Is your name {fullname}? (Y/n) ")
+        if confirmation.upper() == "N" and confirmation != "":
+            fullname = input("Fullname: ")
+        email_to_fullname[email] = fullname
+        # print(email_to_fullname[email])
+        # print(email_to_fullname)
+        email = input('Email: ')
+    for email, fullname in email_to_fullname.items():
+        print(f'{fullname} ({email})')
+
+
+def get_fullname(email):
     name = email.split('@')[0]
     name = name.split('.')
     # print(name)
     fullname = ' '.join(name).title()
-    # print(email)
-    # print(fullname)
-    confirmation = input(f"Is your name {fullname}? (Y/n) ")
-    if confirmation.upper() == "N" and confirmation != "":
-        fullname = input("Fullname: ")
-    email_to_fullname[email] = fullname
-    # print(email_to_fullname[email])
-    # print(email_to_fullname)
-    email = input('Email: ')
+    return fullname
 
-for email, fullname in email_to_fullname.items():
-    print(f'{fullname} ({email})')
+
+main()
