2381e347ed2fea0bb4c76a133ad9cddef0371e4b
lukef <lukeforster6@gmail.com>
2022-10-14 17:31:17 +1000

    Add emails.py

--- Prac5/emails.py
+++ Prac5/emails.py
@@ -1,6 +1,20 @@
-name_to_emails = {}
 
-email = input('What is your name? ')
-print(email)
+email_to_fullname = {}
+email = input('Email: ')
 while email != '':
-    email.
+    name = email.split('@')[0]
+    name = name.split('.')
+    # print(name)
+    fullname = ' '.join(name).title()
+    # print(email)
+    # print(fullname)
+    confirmation = input(f"Is your name {fullname}? (Y/n) ")
+    if confirmation.upper() == "N" and confirmation != "":
+        fullname = input("Fullname: ")
+    email_to_fullname[email] = fullname
+    # print(email_to_fullname[email])
+    # print(email_to_fullname)
+    email = input('Email: ')
+
+for email, fullname in email_to_fullname.items():
+    print(f'{fullname} ({email})')
