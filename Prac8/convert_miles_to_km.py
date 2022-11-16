a5268b93faef8b18c77bfe329c209e6be4a9e4b9
lukef <lukeforster6@gmail.com>
2022-11-16 17:10:01 +1000

    Update convert_miles_to_km.kv and convert_miles_to_km.py to now check for valid inputs, and refactored getting the value for each handle

--- Prac8/convert_miles_to_km.py
+++ Prac8/convert_miles_to_km.py
@@ -13,15 +13,23 @@ class MilesConverterApp(App):
         return self.root
 
     def handle_calculations(self):
-        """ Converts from miles to kilometres """
-        value = float(self.root.ids.input_miles.text)
+        """Converts from miles to kilometres """
+        value = self.get_valid_miles()
         result = value * m_to_km
         self.root.ids.output_label.text = str(result)
 
     def handle_increments(self, change):
-        value = float(self.root.ids.input_miles.text)
+        """Increases the inputted miles by 1 or -1, then calculates the kilometres"""
+        value = self.get_valid_miles()
         self.root.ids.input_miles.text = str(value)
         self.handle_calculations()
+
+    def get_valid_miles(self):
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0
 
 
 MilesConverterApp().run()
