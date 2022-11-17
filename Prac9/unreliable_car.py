b882cb5ca4c9ace8d7779785ac7c65816adbd4ec
lukef <lukeforster6@gmail.com>
2022-11-17 22:03:31 +1000

    Update unreliable_car.py

--- Prac9/unreliable_car.py
+++ Prac9/unreliable_car.py
@@ -1,3 +1,14 @@
+from random import randint
+from Prac9.car import Car
+
+
+class Unreliable(Car):
+    '''An unreliable make of car'''
+
+    def __init__(self, name, fuel, reliability):
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
 
 
 
