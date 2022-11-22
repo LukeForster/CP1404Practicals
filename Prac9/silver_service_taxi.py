fde42a5176249fbb95e2e3ea6835604f5289e23f
lukef <lukeforster6@gmail.com>
2022-11-22 17:51:37 +1000

    Update silver_service to file

--- Prac9/silver_service_taxi.py
+++ Prac9/silver_service_taxi.py
@@ -1,11 +1,16 @@
 from Prac9.taxi import Taxi
 
+
 class SilverServiceTaxi(Taxi):
+    flagfall = 4.5
 
     def __init__(self, name, fuel, fanciness):
         super().__init__(name, fuel)
         self.fanciness = float(fanciness)
         self.price_per_km = self.fanciness * self.price_per_km
 
+    def get_fare_fee(self):
+        fare_fee = self.price_per_km * self.current_fare_distance + self.flagfall
 
-
+    def __str__(self):
+        return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
