96a4fb9f95a8a6f0a2bceee7fcca91610e0368ab
lukef <lukeforster6@gmail.com>
2022-11-30 21:20:24 +1000

    Update testing.py add phrsae_to_sentence function

--- Prac10/testing.py
+++ Prac10/testing.py
@@ -30,7 +30,7 @@ def run_tests():
     # assert test with no message - used to see if the function works properly
     assert repeat_string("Python", 1) == "Python"
     # the test below should fail
-    assert '-'.join(["hi","hi"]) == "hi-hi"
+    assert '-'.join(["hi", "hi"]) == "hi-hi"
     # print('passed')
 
     # 1. fix the repeat_string function above so that it passes the failing test
@@ -42,7 +42,7 @@ def run_tests():
     test_car = Car()
     assert test_car._odometer == 0, "Car does not set odometer correctly"
 
-    # TODO: 2. write assert statements to show if Car sets the fuel correctly
+    # 2. Wrote assert statements to show if Car sets the fuel correctly
     # Note that Car's __init__ function sets the fuel in one of two ways:
     # using the value passed in or the default
     # You should test both of these
@@ -53,21 +53,32 @@ def run_tests():
     assert test_car.fuel == 0
 
 
-run_tests()
 
-# TODO: 3. Uncomment the following line and run the doctests
-# (PyCharm may see your >>> doctest comments and run doctests anyway.)
-doctest.testmod()
 
-# TODO: 4. Fix the failing is_long_word function
+
+
+
+# 4. Fixed the failing is_long_word function
 # (don't change the tests, change the function!)
 
-# TODO: 5. Write and test a function to format a phrase as a sentence,
-# starting with a capital and ending with a single full stop.
-# Important: start with a function header and just use pass as the body
-# then add doctests for 3 tests:
-# 'hello' -> 'Hello.'
-# 'It is an ex parrot.' -> 'It is an ex parrot.'
-# and one more you decide (one that is valid!)
-# test this and watch the tests fail
-# then write the body of the function so that the tests pass
+def phrase_to_sentence(phrase):
+    """format a phrase as a sentence
+    starting with a capital and ending with a single full stop.
+    Important: start with a function header and just use pass as the body
+    t   hen add doctests for 3 tests:
+    'hello' -> 'Hello.'
+    'It is an ex parrot.' -> 'It is an ex parrot.'
+    and one more you decide (one that is valid!)
+    test this and watch the tests fail
+    then write the body of the function so that the tests pass"""
+    sentence = phrase.capitalize()
+    if sentence [-1] != '.':
+        sentence += '.'
+    return sentence
+
+run_tests()
+
+# 3. Uncommented the following line and run the doctests
+# (PyCharm may see your >>> doctest comments and run doctests anyway.)
+
+doctest.testmod()
